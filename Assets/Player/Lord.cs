c6a98c0d9c3e33522f425d2ae8a22e6ce23b93af
Niyazi Emre Akbaş <emreakbas46@outlook.com>
2024-12-20 22:09:06 +0300

    AI started

--- Assets/Player/Lord.cs
+++ Assets/Player/Lord.cs
@@ -4,10 +4,11 @@ public class Lord : MonoBehaviour
 {
     public int gold = 0;
 
+    public int currentMinerCount = 0;
+
     public void AddGold(float amount)
     {
         gold += (int)amount;
-        Debug.Log("Gold: " + gold);
     }
 
     private void Update()
---
+++ Assets/SimpleAI.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleAI : MonoBehaviour
+{
+    public int idealMinerCount = 5;
+
+    public bool isUnderAttack = false;
+
+    public int requiredGold = 100;
+
+    public int requiredGoldForMiner = 50;
+
+    //public float enemyAttackPower = 0;
+
+    [Header("Prefabs")]
+    public GameObject giant;
+    public GameObject shooterFly;
+    public GameObject meleeFly;
+    public GameObject miner;
+
+    [Header("Spawn Points")]
+    public Transform minerPoint;
+    public List<Transform> enemySpawns;
+
+    GameObject[] soldiers;
+
+    private void Awake()
+    {
+        GameObject[] soldiers = { giant, shooterFly, meleeFly };
+
+    }
+
+
+    private void Update()
+    {
+        //Saldýrý Altýnda
+        if (isUnderAttack)
+        {
+            if (hasEnoughSource())
+            {
+                //Produce Soldier
+                ProduceRandomSoldierInRandomPlace();
+
+            }
+            else
+            {
+                //Produce Miner
+                ProduceMiner();
+            }
+        }
+        else
+        {
+            if(hasEnoughMiner())
+            {
+                if (hasEnoughSource())
+                {
+                    //Produce Soldier
+                    ProduceRandomSoldierInRandomPlace();
+                }
+            }
+            else
+            {
+                //Produce Miner
+                ProduceMiner();
+            }
+        }
+    }
+
+    void ProduceRandomSoldierInRandomPlace()
+    {
+        if (enemySpawns.Count == 0)
+            return; // Eðer spawn noktasý yoksa çýk.
+
+        // Rastgele bir spawn noktasý seç
+        Transform spawnPoint = enemySpawns[Random.Range(0, enemySpawns.Count)];
+
+        // Rastgele bir asker türü seç
+        GameObject selectedSoldier = soldiers[Random.Range(0, soldiers.Length)];
+
+        // Askeri spawn et
+        Instantiate(selectedSoldier, spawnPoint.position, spawnPoint.rotation);
+
+        Debug.Log("Hop yeni asker");
+    }
+
+    void ProduceMiner()
+    {
+
+        if (miner == null || minerPoint == null || !hasEnoughSourceForMiner())
+        {
+            print("belkide madenci için paran yoktur he?_");
+            return; // Eðer madenci prefabý ya da spawn noktasý eksikse çýk.
+
+        }
+
+        // Madenciyi spawn et
+        Instantiate(miner, minerPoint.position, minerPoint.rotation);
+
+        // Madenci sayýsýný güncelle (Lord scriptine baðlý)
+        GetComponent<Lord>().currentMinerCount++;
+
+        print("HOp yeni madenci");
+    }
+
+    bool hasEnoughSourceForMiner()
+    {
+        if (GetComponent<Lord>().gold >= requiredGoldForMiner)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+
+        }
+    }
+
+    bool hasEnoughSource()
+    {
+        if(GetComponent<Lord>().gold >= requiredGold)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+
+        }
+    }
+
+    bool hasEnoughMiner()
+    {
+        if (GetComponent<Lord>().currentMinerCount < idealMinerCount)
+        {
+            return true;
+        }
+        return false;
+    }
+}
