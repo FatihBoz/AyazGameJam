4c9c52de505f487f3a7b000703d06b2ca66fceee
Niyazi Emre Akbaş <emreakbas46@outlook.com>
2024-12-22 01:34:51 +0300

    effectCompleted

--- Assets/Skills/DamageArea.cs
+++ Assets/Skills/DamageArea.cs
@@ -1,23 +1,36 @@
 using UnityEngine;
 
-public class FireAreaEffect : BaseSkill
+public class DamageArea : MonoBehaviour
 {
-    [Header("Fire Settings")]
-    public float burnDamage = 10f; // Yanma hasarý
+    public float Damage = 10f; // Yanma hasarý
+    public float radius;
+    public LayerMask targetLayer;
 
-    private void Start()
-    {
-        SetRadius(7f); // Özel yarýçap deðeri
-    }
-
-    public override void ApplyEffect()
+    public void ApplyEffect()
     {
         Collider[] targets = GetObjectsInArea();
 
         foreach (Collider target in targets)
         {
             //Give Damage
+            if(target.gameObject.TryGetComponent<ICombat>(out var enemy))
+            {
+                enemy.TakeDamage(Damage);
+                Debug.Log("Giving Damage to : " + target.gameObject);
+            }
         }
-        PlayParticleEffect();
+    }
+    Collider[] GetObjectsInArea()
+    {
+        return Physics.OverlapSphere(transform.position, radius, targetLayer);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1f, 0.5f, 0f, 0.3f); // Turuncu yarý saydam
+        Gizmos.DrawSphere(transform.position, radius);
+
+        Gizmos.color = Color.red; // Kenar çizgisi için kýrmýzý
+        Gizmos.DrawWireSphere(transform.position, radius);
     }
 }
